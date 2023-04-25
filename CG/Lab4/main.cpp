d045732ad4c89eb2348acb1d72703d6ca60268a2
Maksym-Yurii.Rudko <maksym-iurii.rudko.pz.2020@lpnu.ua>
2023-04-25 23:54:21 +0300

    [CG-4] Finished the lab

--- CG/Lab4/main.cpp
+++ CG/Lab4/main.cpp
@@ -1,5 +1,3 @@
-#include "main.h"
-
 #include <iostream>
 
 #include <GL/glut.h>
@@ -9,26 +7,20 @@
 #include <thread>
 
 using namespace std::chrono_literals;
-GLubyte texture_image[64][64][3];
+GLuint checkerboardTexture;
 
-void drawCube(long double timeSinceLastFrameMs);
+int fogIndex = 0;
+
+void initCheckerboardTexture();
+
+void windowResize(int width, int height);
+
+void refreshDisplay();
+
+void onKeyDown(unsigned char character, int x, int y);
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
-
-    for (int i = 0; i < 64; i++) {
-        for (int j = 0; j < 64; j++) {
-            if ((i / 4 + j / 4) % 2 == 0) {
-                texture_image[i][j][0] = 255; // R component
-                texture_image[i][j][1] = 255; // G component
-                texture_image[i][j][2] = 255; // B component
-            } else {
-                texture_image[i][j][0] = 0; // R component
-                texture_image[i][j][1] = 0; // G component
-                texture_image[i][j][2] = 0; // B component
-            }
-        }
-    }
 
     glutInitWindowSize(1280, 580);
     glutInitWindowPosition(120, 80);
@@ -48,90 +40,112 @@ int main(int argc, char **argv) {
     glEnable(GL_LIGHTING);
     glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-    glEnable(GL_COLOR_MATERIAL);
-
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glClearColor(0.5f, 0.5f, 0.8f, 1.0f);
 
-    const float lightPos[]{0.f, 20.f, -5.f, 1.f};
-    const float specular[]{1.f, 1.f, 0.7f, 1};
-    const float diffuse[]{3.f, 0.f, 3.f, 10};
-    const float ambient[]{0.2f, 0.2f, 0.2f, 1};
-    const float direction[]{0.5f, -1.f, -0.2f};
+    const float lightPos[]{-3.f, 12.f, 5.f, 1.f};
+    const float direction[]{0.2f, -1.f, -0.9f};
+    /*const float specular[]{0.4f, .4f, 0.4f, 1};
+    const float diffuse[]{0.4f, 0.f, 0.1f, 1};
+    const float ambient[]{0.05f, 0.05f, 0.15f, 1};*/
+    const float specular[]{0.4f, .4f, 0.4f, 1};
+    const float diffuse[]{0.4f, .4f, 0.4f, 1};
+    const float ambient[]{0.05f, 0.05f, 0.05f, 1};
+
     glLightfv(GL_LIGHT1, GL_POSITION, lightPos);
     glLightfv(GL_LIGHT1, GL_SPECULAR, specular);
-    //glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse);
     glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, direction);
-    glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 16);
-    glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 90);
-    //glLightfv(GL_LIGHT1, GL_AMBIENT, ambient);
+    glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 2);
+    glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 10);
+    glLightfv(GL_LIGHT1, GL_AMBIENT, ambient);
 
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
 
     glEnable(GL_LIGHT1);
+
+    initCheckerboardTexture();
 
     glutMainLoop();
 
     return 0;
 }
 
-
-// Function to draw the cone
 void drawCone() {
-    // Set the color to green
+    const float redColorDiffuse[]{1.f, 0.f, 0.f};
+    const float redColorSpecular[]{1.f, .3f, .1f};
 
-    const float redCubeColor[]{1.f, 1.f, 1.f};
+    glEnable(GL_COLOR_MATERIAL);
 
     glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, redCubeColor);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, redCubeColor);
-    glColor3f(1.f, 1.f, 1.f);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, redColorSpecular);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, redColorDiffuse);
+    glColor3f(1, 0, 0);
 
     glPushMatrix();
     glRotated(90, -1, 0, 0);
 
-    // Draw the cone
-    glutSolidCone(2.0f, 4.0f, 30, 30);
+    glutSolidCone(2.0f, 4.0f, 64, 64);
     glPopMatrix();
+
+    glDisable(GL_COLOR_MATERIAL);
 }
 
-// Function to draw the cylinder
 void drawCylinder() {
-    // Set the texture for the cylinder
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 64, 64, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_image);
+    glColor3f(1, 1, 1);
 
+    glEnable(GL_COLOR_MATERIAL);
 
     glPushMatrix();
     glTranslated(-4, 1, 0);
     glRotated(90, 0, 1, 0);
 
+    glBindTexture(GL_TEXTURE_2D, checkerboardTexture);
 
-    // Draw the cylinder with texture
     glEnable(GL_TEXTURE_2D);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    glColor3f(1, 1,1);
     GLUquadricObj *cylinder = gluNewQuadric();
     gluQuadricTexture(cylinder, GL_TRUE);
-    gluCylinder(cylinder, 1.0f, 1.0f, 4.0f, 30, 30);
+    gluCylinder(cylinder, 1.0f, 1.0f, 4.0f, 48, 48);
     gluDeleteQuadric(cylinder);
     glDisable(GL_TEXTURE_2D);
 
-    glPopMatrix();
-}
-
-void render(long double elapsedMs) {
-    //drawCube(elapsedMs);
+    glBindTexture(GL_TEXTURE_2D, -1);
 
     glPushMatrix();
+
+    gluQuadricOrientation(cylinder, GLU_OUTSIDE);
+    gluDisk(cylinder, 0, 1, 48, 8);
+
+    glPopMatrix();
+
+    glPopMatrix();
+
+    glDisable(GL_COLOR_MATERIAL);
+}
+
+void render(float totalTimeSeconds, long double frameTimeS) {
+    static const float fogColor[]{.5f, .6f, .7f};
+
+    glEnable(GL_FOG);
+    glFogi(GL_FOG_MODE, fogIndex == 0 ? GL_LINEAR : (GL_EXP + fogIndex % 2));
+    glFogf(GL_FOG_START, 6);
+    glFogf(GL_FOG_END, 20);
+    glFogf(GL_FOG_DENSITY, 0.03);
+    glFogfv(GL_FOG_COLOR, fogColor);
+
+    float rotationAngleY = fmodf(totalTimeSeconds * 120, 360);
+    float rotationAngleZ = fmodf(totalTimeSeconds * 90, 360);
+
+    glPushMatrix();
+
     glTranslatef(1, -3, -10);
+
+    glRotatef(rotationAngleY, 0, 1, 0);
+    glRotatef(rotationAngleZ, 1, 0, 0);
 
     drawCone();
     drawCylinder();
@@ -140,6 +154,8 @@ void render(long double elapsedMs) {
 }
 
 void refreshDisplay() {
+    static long double timeMs = 0;
+
     static auto currentFrameTime = std::chrono::steady_clock::now();
     static auto lastFrameTime = currentFrameTime;
 
@@ -152,7 +168,11 @@ void refreshDisplay() {
 
     long double elapsedMs = static_cast<long>(elapsedDurationNs.count()) / 1000000.0;
 
-    render(elapsedMs);
+    timeMs += elapsedMs;
+
+    auto timeS = static_cast<float>(timeMs / 1000);
+
+    render(timeS, elapsedMs);
 
     lastFrameTime = currentFrameTime;
 
@@ -161,53 +181,30 @@ void refreshDisplay() {
     std::this_thread::sleep_for(1ms);
 }
 
-void drawCube(long double timeSinceLastFrameMs) {
-    static long double timeMs = 0;
+void initCheckerboardTexture() {
+    GLubyte texture_image[64][64][3];
 
-    timeMs += timeSinceLastFrameMs;
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 64; j++) {
+            if ((i / 4 + j / 4) % 2 == 0) {
+                texture_image[i][j][0] = 255;
+                texture_image[i][j][1] = 255;
+                texture_image[i][j][2] = 255;
+            } else {
+                texture_image[i][j][0] = 0;
+                texture_image[i][j][1] = 0;
+                texture_image[i][j][2] = 0;
+            }
+        }
+    }
 
-    auto timeS = static_cast<float >(timeMs / 1000);
+    glGenTextures(1, &checkerboardTexture);
+    glBindTexture(GL_TEXTURE_2D, checkerboardTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 64, 64, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_image);
 
-    const float a = 1.5;
-    const float xRange = 5;
-
-    float rotationAngle = fmodf(timeS * 180, 360);
-
-    float x = xRange * (fmodf(timeS / 2, 1) - 0.5f);
-
-
-    glPushMatrix();
-
-    glTranslatef(x + 1, -3, -10 - a * x);
-    glPushMatrix();
-    glRotatef(rotationAngle, 0, 1, 0);
-
-    const float redCubeColor[]{0.4f, 0.f, 0.1f, 2.f};
-
-    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, redCubeColor);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, redCubeColor);
-    glMaterialf(GL_FRONT, GL_SHININESS, 4);
-    glColor3f(0.8, 0, 0);
-
-    glutSolidCube(2);
-    glPopMatrix();
-
-    glColor3f(0.8, 0, 0.8);
-    glTranslated(-4, 0, 0);
-
-    glPushMatrix();
-    glRotatef(rotationAngle, 0, 1, 0);
-    glutWireCube(3);
-
-    glPopMatrix();
-
-    glColor3f(0, 0, 0.6);
-    glTranslated(8, 0, 0);
-
-    glutSolidSphere(2, 32, 32);
-
-    glPopMatrix();
+    glBindTexture(GL_TEXTURE_2D, -1);
 }
 
 void windowResize(GLint newWidth, GLint newHeight) {
@@ -230,5 +227,9 @@ void onKeyDown(unsigned char character, int x, int y) {
     switch (character) {
         case 27:        // escape
             exit(0);
+            break;
+        case 'f':
+            fogIndex = (fogIndex + 1) % 3;
+            break;
     }
 }
